b9cf5e54414c3883181b8fb6fe77acc8f00a718d
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-12-18 15:43:31 +0000

    added endorsememnts controller - failing test

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   
 
 
-  resources :restaurants do
-    resources :reviews
+  resources :restaurants, shallow: true do
+    resources :reviews do
+      resources :endorsements
+    end
   end
 
   root to: "restaurants#index"
