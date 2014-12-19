142be2e29ba49ac08905beec597c47d1b3c1b23e
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-12-19 15:59:03 +0000

    tried updating amazon settings for european server

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,7 +4,10 @@ class Restaurant < ActiveRecord::Base
   validates :name, length: {minimum: 3}, uniqueness: true
 
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images.:style/missing.png"
+  has_attached_file :image, 
+                    :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
+                    :default_url => "/images.:style/missing.png", 
+                    :s3_options => { :server => "s3-eu-west-1.amazonaws.com"  }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   def average_rating
     return 'N/A' if reviews.none?
