7fbe6bf64945730958f2a13e6609784b8b647f54
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-12-20 07:42:19 +0000

    paperclip not working with AWS

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -6,8 +6,8 @@ class Restaurant < ActiveRecord::Base
 
   has_attached_file :image, 
                     :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
-                    :default_url => "/images.:style/missing.png", 
-                    :s3_options => { :server => "s3-eu-west-1.amazonaws.com"  }
+                    :default_url => "/images.:style/missing.png" 
+                    # ,:s3_options => { :server => "s3-eu-west-1.amazonaws.com"  }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   def average_rating
     return 'N/A' if reviews.none?
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# AWS::S3::DEFAULT_HOST = "s3-eu-west-1.amazonaws.com"
 # Initialize the Rails application.
 Rails.application.initialize!
